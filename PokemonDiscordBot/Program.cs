661ae97a4a64b55323d0e211f3460e819bb5c7f0
byBlurr <dbldumont11@gmail.com>
2021-05-16 12:32:12 +0100

    Add project files.

---
+++ PokemonDiscordBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PokemonDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
